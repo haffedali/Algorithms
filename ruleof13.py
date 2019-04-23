99a3592fd2c5a2214c167ad5c4d5f39bef32c189
Hoff <haffedmcnair@gmail.com>
2019-04-23 13:10:23 -0700

    fun rule of division... hopefully it comes in handy

---
+++ ruleof13.py
@@ -0,0 +1,39 @@
+def thirt(n):
+    # lines 3...8 could be done in one line if using commas to seperate variables
+    rule = [1,10,9,12,3,4]
+    result1 = 0
+    result2 = 1
+    rollingSum = 0
+    count = 0
+    num = numToArrStr(n)
+    status = "result1"
+    while result2 != result1:
+        
+        for i in num:
+            rollingSum += int(i)*rule[count]
+            count += 1
+            if count > 5:
+                count = 0
+        count = 0
+        if status == "result1":
+            result1 = rollingSum
+            num = numToArrStr(result1)
+            rollingSum = 0
+            status = "result2"
+        else:
+            result2 = rollingSum
+            num = numToArrStr(result2)
+            rollingSum = 0
+            status = "result1"
+    return result1
+def numToArrStr(n):
+    n = str(n)
+    num = []
+    for i in n:
+        num[:0] = i
+    return num
+
+
+
+
+thirt(1234567)
